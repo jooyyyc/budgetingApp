6be31140dc0acf77ac4bfcccc081c360702eca0a
Joy Chang <joymchang@gmail.com>
2023-07-13 14:00:29 -0700

    Initial Commit

---
+++ budgetingApp/budgetingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetingAppApp.swift
+//  budgetingApp
+//
+//  Created by scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct budgetingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
